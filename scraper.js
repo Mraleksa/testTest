f2013481ffa062db1ee0bbcba6afa85ce67cf31c
Mraleksa <a.gribanovskiy@gmail.com>
2017-11-06 14:53:54 +0200

    Update scraper.js

--- scraper.js
+++ scraper.js
@@ -44,7 +44,7 @@ function run(db) {
 	fetchPage("http://w1.c1.rada.gov.ua/pls/radan_gs09/ns_golos?g_id=15122", function (body) {
 		// Use cheerio to find things in the page with css selectors.
 		var $ = cheerio.load(
-			`body`,
+			body,
 			 { decodeEntities: false }
 				    );
 
